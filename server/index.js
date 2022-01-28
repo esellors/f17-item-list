8891b5bfe46357f17cffaab1314c4a5747ceb82c
Eric Sellors <github@esellors.com>
2022-01-28 14:37:43 -0600

    post and delete

---
+++ server/index.js
@@ -0,0 +1,58 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+
+
+app.use(cors());
+
+app.use(express.json()); // When we want to be able to accept JSON.
+
+app.get("/api/compliment", (req, res) => {
+  const compliments = ["Gee, you're a smart cookie!",
+					 "Cool shirt!",
+					 "Your Javascript skills are stellar.",
+  ];
+
+  // choose random compliment
+  let randomIndex = Math.floor(Math.random() * compliments.length);
+  let randomCompliment = compliments[randomIndex];
+
+  res.status(200).send(randomCompliment);
+  
+});
+
+const items = []
+let id = 1;
+
+app.post('/api/items', (req, res) => {
+  const { item } = req.body;
+
+  let newItem = {
+    id,
+    item
+  };
+
+  items.push(newItem)
+  id++
+
+  res.status(200).send(items);
+})
+
+app.delete('/api/items/:itemId', (req, res) => {
+  const itemId = +req.params.itemId;
+
+  console.log(itemId)
+
+  const tgtInd = items.findIndex(itemObj => {
+    return itemObj.id === itemId;
+  })
+
+  const removed = items.splice(tgtInd, 1)
+
+  res.status(200).send([removed[0], items])
+
+})
+
+
+app.listen(4000, () => console.log("Server running on 4000"));
